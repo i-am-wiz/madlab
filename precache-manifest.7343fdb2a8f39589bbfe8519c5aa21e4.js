c5e82ef5eaf98a230937074ecd9d09222a31e1d4
Ved Timbadiya <63511301+i-am-wiz@users.noreply.github.com>
2023-05-20 14:47:12 +0530

    Add files via upload

---
+++ precache-manifest.7343fdb2a8f39589bbfe8519c5aa21e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba1cf21fa8b8c6069722e041f24154a7",
+    "url": "/madlab/index.html"
+  },
+  {
+    "revision": "467f85b604a3f1524c26",
+    "url": "/madlab/static/css/2.9be35272.chunk.css"
+  },
+  {
+    "revision": "d93086e8d3b5896071e2",
+    "url": "/madlab/static/css/main.38f38865.chunk.css"
+  },
+  {
+    "revision": "467f85b604a3f1524c26",
+    "url": "/madlab/static/js/2.903e3fb3.chunk.js"
+  },
+  {
+    "revision": "d93086e8d3b5896071e2",
+    "url": "/madlab/static/js/main.842c4c05.chunk.js"
+  },
+  {
+    "revision": "396f417b8273cab40f0e",
+    "url": "/madlab/static/js/runtime~main.05b0808b.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/madlab/static/media/logo.9f7e506d.svg"
+  }
+]);
